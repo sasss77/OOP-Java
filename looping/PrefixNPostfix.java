a089fc3a493685a66eaed2b956386920abdfae46
Saugat Paneru <saugatpaneru40@gmail.com>
2024-11-14 11:10:31 +0545

    looping folder added with for loop

---
+++ looping/PrefixNPostfix.java
@@ -0,0 +1,23 @@
+
+public class prefixNpostfix {
+    public static void main(String[] args) {
+        int  a = 10;
+        
+        // Prefix increment operator
+        System.out.println("Before increment: " + a);
+        System.out.println("After increment: " + ++a);
+        
+        // Prefix decrement operator
+        System.out.println("Before decrement: " + a);
+        System.out.println("After decrement: " + --a);
+        
+        // Postfix increment operator
+        System.out.println("Before increment: " + a);
+        System.out.println("After increment: " + a++);
+        
+        // Postfix decrement operator
+        System.out.println("Before decrement: " + a);
+        System.out.println("After decrement: " + a--);
+    }
+    
+}
